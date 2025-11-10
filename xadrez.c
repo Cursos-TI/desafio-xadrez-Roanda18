19f57019053da1a97577afff8a7439c6d5b8ff3a
Matheus Peixoto Matos <roanda18@gmail.com>
2025-11-10 11:23:08 -0300

    nivel mestre

--- xadrez.c
+++ xadrez.c
@@ -3,50 +3,64 @@
 // Desafio de Xadrez - MateCheck
 // Este código inicial serve como base para o desenvolvimento do sistema de movimentação das peças de xadrez.
 // O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
+void moverTorre(int movimentosRestantes)
+{
+    if (movimentosRestantes > 0)
+    {
+        printf("Direita \n");
+        moverTorre(movimentosRestantes - 1);
+    }
+}
+
+void moverBispo(int movimentosRestantes)
+{
+    if (movimentosRestantes > 0)
+    {
+        printf("Cima \n");
+        for ( int i = 0; i < 1; i++)
+        {
+            printf("Direita \n");
+        }
+        moverBispo(movimentosRestantes - 1);
+    }    
+}
+
+void moverRainha(int movimentosRestantes)
+{
+    if (movimentosRestantes > 0)
+    {
+        printf("Esquerda \n");
+        moverRainha(movimentosRestantes - 1);
+    }
+}
 
 int main()
 {
-    const int torreMovimentos = 5;
-    int bispoMovimentos = 0;
-    int rainhaMovimentos = 0;
-    int cavaloMovimentos = 0;
+    int i, j;
 
     printf("Movimentação da Torre: \n");
-    for (size_t i = 0; i <= torreMovimentos; i++)
-    {
-        printf("Direita \n", i + 1);
-    }
+    moverTorre(5);
     printf("\n");
-    
+
     printf("Movimentação do Bispo: \n");
-    while (bispoMovimentos < 5)
-    {
-        printf("Cima, Direita \n");
-        bispoMovimentos++;
-    }
+    moverBispo(5);
     printf("\n");
 
     printf("Movimentação da Rainha: \n");
-    do
-    {
-        printf("Esquerda \n");
-        rainhaMovimentos++;
-    } while (rainhaMovimentos < 8);
+    moverRainha(8);
     printf("\n");
 
     printf("Movimentação do Cavalo: \n");
-    for (size_t i = 0; i < 1; i++)
-    {        
-        while (cavaloMovimentos < 2)
+    for (i = 0; i < 1; i++)
+    {
+        for (j = 0; j < 2; j++)
         {
-            printf("Baixo \n");
-            cavaloMovimentos++;
-        }   
-        printf("Esquerda \n");                 
-        
+            printf("Cima \n");
+        }
+
+        printf("Direita \n");
     }
     printf("\n");
-
 
     // Nível Novato - Movimentação das Peças
     // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
